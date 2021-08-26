4e14d33aad20e02bc46f5348cf3f625eb47bebc4
Koushik Paul <koushik.paul1999@gmail.com>
2021-08-26 12:01:05 +0530

    first commit

---
+++ force-app/main/default/lwc/toDoListItem/toDoListItem.js
@@ -0,0 +1,17 @@
+/**
+ * ToDoListItem component
+ * Represents an item with some styling based on item status
+ */
+import { LightningElement, api } from "lwc";
+
+export default class ToDoListItem extends LightningElement {
+  //public properties to get todo information
+  @api todoId;
+  @api todoName;
+  @api done = false;
+
+  //get property to return container class based on todo status
+  get containerClass() {
+    return this.done ? "todo completed" : "todo upcoming";
+  }
+}
